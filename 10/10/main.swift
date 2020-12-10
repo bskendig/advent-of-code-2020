9534a7810b9c96c7c622769874c998177979f6c2
Brian Kendig <brian@mac.com>
2020-12-10 06:17:26 -0500

    10 part 2

--- 10/10/main.swift
+++ 10/10/main.swift
@@ -12,17 +12,41 @@ func getInput() -> String {
     return try! String(contentsOfFile: path!)
 }
 
+var jolts: [Int] = []
+var alreadyCounted: [Int: Int] = [:]
+
+func howManySolutionsFrom(index: Int) -> Int {
+    if let ac = alreadyCounted[index] {
+        return ac
+    }
+    let current = jolts[index]
+    var total = 0
+    for i in index+1 ... index+3 {
+        if i >= jolts.count {
+            break
+        }
+        if jolts[i] - current <= 3 {
+            total += howManySolutionsFrom(index: i)
+        }
+    }
+    alreadyCounted[index] = total
+    return total
+}
+
 func main() {
-    let jolts = getInput().split(separator: "\n").map({ Int($0)! }).sorted()
+    jolts = getInput().split(separator: "\n").map { Int($0)! }
+    jolts.append(0)
+    jolts = jolts.sorted()
+    alreadyCounted = [(jolts.count - 1): 1]
     var count = [0, 0, 0, 0]  // we'll only use offsets 1 and 3
     count[1] = 0
-    count[3] = 1  // for the end adapter
-    count[jolts[0]] += 1
+    count[3] = 1
     for i in 1 ..< jolts.count {
         let diff = jolts[i] - jolts[i-1]
         count[diff] += 1
     }
     print(count[1] * count[3])
+    print(howManySolutionsFrom(index: 0))
 }
 
 main()
